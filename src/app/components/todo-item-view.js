a77ca7bcab3dbf7538f06150345e8fdf2364b7ec
Frank Batista <nybatista@gmail.com>
2025-01-06 18:42:15 -0500

    optimized todos

--- src/app/components/todo-item-view.js
+++ src/app/components/todo-item-view.js
@@ -5,40 +5,33 @@ import TodoItemTmpl from './templates/todo-item.tmpl.html';
 export class TodoItemView extends ViewStream {
   constructor(props = {}) {
     props.channels = ['CHANNEL_UI'];
-    // Attach the TodoTraits so we can call todos$ methods
+    props.class = 'todo-item';
     props.traits = [TodoTraits];
     props.template = TodoItemTmpl;
     super(props);
-
-    this.state = {
-      editing: false,
-    };
   }
 
   broadcastEvents() {
-    // Each item’s edit / remove / end-edit button
-    // will broadcast a click to the “TODO_CHANNEL”
+    // Broadcast all button clicks
     return [['button', 'click']];
   }
 
   addActionListeners() {
-    // Only respond if payload.id matches this item’s id,
-    // and action is one of "edit", "remove", or "endEdit"
+    // Filter so only events with the correct todoId reach onItemEvent
     const todoFilter = new ChannelPayloadFilter({
       todoId: this.props.data.todoId,
     });
-
     return [['CHANNEL_UI_CLICK_EVENT', 'onItemEvent', todoFilter]];
   }
 
   onItemEvent(e) {
     const { action } = e.payload;
-    if (action === 'edit') {
-      this.todos$StartEditMode();
-    } else if (action === 'remove') {
-      this.todos$RemoveItem();
-    } else if (action === 'endEdit') {
-      this.todos$EndEditMode();
-    }
+    const actionsFnLookup = {
+      edit: this.todos$StartEditMode,
+      remove: this.todos$RemoveItem,
+      endEdit: this.todos$EndEditMode,
+    };
+    const fn = actionsFnLookup[action];
+    if (fn) fn.call(this);
   }
 }
--- src/app/traits/todo-traits.js
+++ src/app/traits/todo-traits.js
@@ -2,46 +2,30 @@ import { SpyneTrait } from 'spyne';
 
 export class TodoTraits extends SpyneTrait {
   constructor(context) {
-    // "todos$" enforces consistent naming for trait methods
+    // "todos$" prefix ensures consistent naming for trait methods
     let traitPrefix = 'todos$';
     super(context, traitPrefix);
   }
 
-  /**
-   * Switch from display to edit mode:
-   *  - Show an <input> with the current text
-   *  - Possibly hide the normal <p> text
-   */
   static todos$StartEditMode() {
-    this.state.editing = true;
-    // Show input, hide .todo-text
-    this.props.el$('.todo-text').el.style.display = 'none';
-    this.props.el$('.edit-input').el.style.display = 'inline-block';
+    // Toggle .is-editing class on the root .todo-item element
+    this.props.el$.toggle('is-editing', true);
+
+    // Initialize the edit input text
     this.props.el$('.edit-input').el.value = this.props.data.text;
-    this.props.el$('.end-edit').el.style.display = 'inline-block';
   }
 
-  /**
-   * End edit mode:
-   *  - Capture input text and update the item’s data
-   *  - Revert the UI to display mode
-   */
   static todos$EndEditMode() {
-    this.state.editing = false;
+    this.props.el$.toggle('is-editing', false);
+
+    // If we have new text, update the data
     const newVal = this.props.el$('.edit-input').el.value.trim();
     if (newVal) {
       this.props.data.text = newVal;
       this.props.el$('.todo-text').el.innerText = newVal;
     }
-    // Hide input, show .todo-text
-    this.props.el$('.todo-text').el.style.display = 'inline-block';
-    this.props.el$('.edit-input').el.style.display = 'none';
-    this.props.el$('.end-edit').el.style.display = 'none';
   }
 
-  /**
-   * Dispose the current Todo item
-   */
   static todos$RemoveItem() {
     this.disposeViewStream();
   }
